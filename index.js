db40a9636e5cf53df323ed8e256bd2627732a903
maythinn <90564038+developer-maythinn@users.noreply.github.com>
2023-11-24 14:24:51 +0630

    generate tokens by style-dictionary

--- index.js
+++ index.js
@@ -7,6 +7,35 @@ const generateTokens = ({ sourceGlob, buildPath, destination }) => {
     // source: "tokens/!(light.json)"
     source: [sourceGlob], // exclude light theme
     // source: ["tokens/testing.json"], // exclude light theme
+    format: {
+      // Adding a custom format to show how to get an alias's name.
+      customFormat: function ({ dictionary, options }) {
+        let aa = dictionary.allTokens
+          .map((token) => {
+            let value = JSON.stringify(token.value);
+            // new option added to decide whether or not to output references
+            if (options.outputReferences) {
+              // the `dictionary` object now has `usesReference()` and
+              // `getReferences()` methods. `usesReference()` will return true if
+              // the value has a reference in it. `getReferences()` will return
+              // an array of references to the whole tokens so that you can access
+              // their names or any other attributes.
+              if (dictionary.usesReference(token.original.value)) {
+                const refs = dictionary.getReferences(token.original.value);
+                refs.forEach((ref) => {
+                  value = value.replace(ref.value, function () {
+                    return `${ref.name}`;
+                  });
+                });
+              }
+            }
+
+            return ` ${token.name}: ${value},`;
+          })
+          .join(`\n`);
+        return `module.exports ={ ${aa}}`;
+      },
+    },
     platforms: {
       js: {
         transformGroup: "js",
@@ -14,7 +43,8 @@ const generateTokens = ({ sourceGlob, buildPath, destination }) => {
         files: [
           {
             destination: destination,
-            format: "javascript/es6",
+            // format: "javascript/es6",
+            format: "customFormat",
           },
         ],
       },
@@ -23,6 +53,15 @@ const generateTokens = ({ sourceGlob, buildPath, destination }) => {
 
   builder.buildAllPlatforms();
 };
+
+// module.exports = generateTokens({
+//   sourceGlob: "tokens/!(light.json)",
+//   destination: "darkMode.js",
+// }); // dark mode
+// module.exports = generateTokens({
+//   sourceGlob: "tokens/!(dark.json)",
+//   destination: "lightMode.js",
+// }); // light mode
 
 generateTokens({
   sourceGlob: "tokens/!(light.json)",
